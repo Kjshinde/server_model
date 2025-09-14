bd996fe0ee1bcc2d8fcdd07ba887872bc2a0952b
kshinde6 <kshinde6@crtvc102macj01>
2025-09-14 14:28:18 -0700

    testing CCs sep 14

--- load_callback.m
+++ load_callback.m
@@ -1,6 +1,6 @@
 classdef load_callback
 
-    methods(Static) % testing MATLAB commit
+    methods(Static) % testing CCs commit (14 sep) 
 
         % Use the code browser on the left to add the callbacks.
 
